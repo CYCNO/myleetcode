b7d7fdd469a18aedc0fe5aeb9659c2ace5aa577e
CYCNO <90704569+CYCNO@users.noreply.github.com>
2023-05-17 23:15:17 +0530

    Update 347-Top K Frequent Elements.py

--- Arrays & Hashing/347-Top K Frequent Elements.py
+++ Arrays & Hashing/347-Top K Frequent Elements.py
@@ -1,15 +1,21 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = {}
-        freq = [[] for i in range(len(nums) + 1)]
+        count = {}  # Dictionary to store the count of each number ({1: 3, 2: 2, 3: 1})
+        freq = [[] for i in range(len(nums) + 1)]  # List of lists to store numbers grouped by their frequency ([[], [3], [2], [1], [], [], []])
 
+        # Count the occurrences of each number
         for n in nums:
             count[n] = 1 + count.get(n, 0)
+
+        # Group numbers by their frequency
         for n, c in count.items():
             freq[c].append(n)
-        res = []    
+
+        res = []  # List to store the top k frequent numbers
+        # Traverse the frequencies in descending order
         for i in range(len(freq) - 1, 0, -1):
+            # Traverse the numbers with the current frequency
             for n in freq[i]:
-                res.append(n)
-                if len(res) == k:
-                    return res
+                res.append(n)  # Add the number to the result
+                if len(res) == k:  # If we have found the top k frequent numbers, return the result
+                    return res # [1, 2]
