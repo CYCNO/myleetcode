3d45282aaabc8e96f34c327846735d15270d8b78
CYCNO <90704569+CYCNO@users.noreply.github.com>
2025-12-31 15:19:21 +0530

    Implement groupAnagrams method in Solution class
    Refactor groupAnagrams method to sort and group anagrams efficiently.

--- Arrays & Hashing/49-Group Anagrams.py
+++ Arrays & Hashing/49-Group Anagrams.py
@@ -8,3 +8,32 @@ class Solution:
             else:
                 dic[sortWord] = [i] # adding the new sorted word like key
         return list(dic.values())           
+
+# My Attempt with no help
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # make all elem sorted in alphabetical order and append it with their index number
+        c = []
+        for i, elem in enumerate(strs):
+            c.append(["".join(sorted(elem)), i])
+
+        d=sorted(c) #sort all the sorted value list
+        f =[] # declare final list
+
+        i=0
+
+        #loop through all the elem in d
+        while i < len(d):
+            temp = [strs[d[i][1]]] # create a temp list and init a the first string 
+            #loop until either i get out of range or the sorted chars dont match any more
+            while i < len(d) - 1 and d[i][0] == d[i+1][0]:
+                temp.append(strs[d[i+1][1]])
+                i += 1
+            
+            # push the temp to the final list
+            f.append(temp)
+            i+=1
+        
+        # return final list
+        return f
